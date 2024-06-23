44d2a9bb2eefce8bd511d6605b3a6e704b5ea88b
Meekyle Mondal <amond049@uottawa.ca>
2024-06-23 16:04:32 -0400

    Trying something new for authorization

--- recently-listened/lambda_function.py
+++ recently-listened/lambda_function.py
@@ -4,6 +4,8 @@ import json
 from botocore.exceptions import ClientError
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+# from spotipy.oauth2 import SpotifyClientCredentials
+
 
 # This is the link to the Lambda https://ebc2llbzrd4v43kqzwc6ctw2cy0jqjzb.lambda-url.us-east-2.on.aws/
 def lambda_handler(event, context):
@@ -23,19 +25,56 @@ def lambda_handler(event, context):
     except ClientError as e:
         raise e
 
-    CLIENT_ID = get_response['SecretString']['CLIENT_ID']
-    CLIENT_SECRET = get_response['SecretString']['CLIENT_SECRET']
-    REDIRECT_URI = get_response['SecretString']['REDIRECT_URI']
+    secrets_as_string = get_response['SecretString']
+    split_secrets = secrets_as_string.split(",")
+    client_id = ""
+    client_secret = ""
+    redirect_uri = ""
+
+    for string in split_secrets:
+        each_string = string.split(":")
+        if each_string[0] == '{"CLIENT_ID"':
+            client_id = each_string[1]
+        elif each_string[0] == '"CLIENT_SECRET"':
+            client_secret = each_string[1]
+        elif each_string[0] == '"REDIRECT_URI"':
+            combined_url = each_string[1] + each_string[2] + each_string[3]
+            # Requires some further processing unfortunately
+            redirect_uri = combined_url.split("}")[0]
+
+    CLIENT_ID = client_id
+    CLIENT_SECRET = client_secret
+    REDIRECT_URI = redirect_uri
 
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
                                                    client_secret=CLIENT_SECRET,
                                                    redirect_uri=REDIRECT_URI,
-                                                   scope="user-read-recently-played"))
+                                                   scope="user-read-recently-played",
+                                                   open_browser=False))
 
-    result = sp.current_user_recently_played(limit = 20)
+    result = sp.current_user_recently_played(limit=20)
 
     print(result)
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda! This is the second one')
     }
+
+'''
+def main():
+    id = "3646292c293e4267bb6c48ab048d6a0c"
+    secret = "91bf3b806b744a0ebefe82ecb3229c74"
+    uri = "http://localhost:1234/"
+
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=id,
+                                                   client_secret=secret,
+                                                   redirect_uri=uri,
+                                                   scope="user-read-recently-played",
+                                                   open_browser=False))
+
+    result = sp.current_user_recently_played(limit=20)
+
+    print(result)
+
+main()
+'''
