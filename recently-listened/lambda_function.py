65a441b4325991b87b74f8c5b916155b6e89caa5
Meekyle Mondal <amond049@uottawa.ca>
2024-06-23 23:56:08 -0400

    Trying to create a simple Flask application and seeing if that works

--- recently-listened/lambda_function.py
+++ recently-listened/lambda_function.py
@@ -4,61 +4,22 @@ import json
 from botocore.exceptions import ClientError
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-# from spotipy.oauth2 import SpotifyClientCredentials
+from flask import Flask
+
+# Thinking of using a Flask application to navigate the authorization
+app = Flask(__name__)
 
 
 # This is the link to the Lambda https://ebc2llbzrd4v43kqzwc6ctw2cy0jqjzb.lambda-url.us-east-2.on.aws/
 def lambda_handler(event, context):
-    secret_name = "mickeys-marvels/spotipy"
-    region = "us-east-2"
-
-    session = boto3.session.Session()
-    client = session.client(
-        service_name='secretsmanager',
-        region_name=region
-    )
-
-    try:
-        get_response = client.get_secret_value(
-            SecretId=secret_name
-        )
-    except ClientError as e:
-        raise e
-
-    secrets_as_string = get_response['SecretString']
-    split_secrets = secrets_as_string.split(",")
-    client_id = ""
-    client_secret = ""
-    redirect_uri = ""
-
-    for string in split_secrets:
-        each_string = string.split(":")
-        if each_string[0] == '{"CLIENT_ID"':
-            client_id = each_string[1]
-        elif each_string[0] == '"CLIENT_SECRET"':
-            client_secret = each_string[1]
-        elif each_string[0] == '"REDIRECT_URI"':
-            combined_url = each_string[1] + each_string[2] + each_string[3]
-            # Requires some further processing unfortunately
-            redirect_uri = combined_url.split("}")[0]
-
-    CLIENT_ID = client_id
-    CLIENT_SECRET = client_secret
-    REDIRECT_URI = redirect_uri
-
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
-                                                   client_secret=CLIENT_SECRET,
-                                                   redirect_uri=REDIRECT_URI,
-                                                   scope="user-read-recently-played"
-                                                   ))
-
-    result = sp.current_user_recently_played(limit=20)
-
-    print(result)
+    app.run('localhost', 8080)
+    print("Running Flask application")
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda! This is the second one')
     }
 
 # May have to use some requests to get the authorization working
-
+@app.route("/")
+def test():
+    return "Hello World!"
