c30b61bf58c202d8addce233c8bacc4321ea8d93
Meekyle Mondal <amond049@uottawa.ca>
2024-06-24 15:31:09 -0400

    I think I've got the basics of the flask app set up

--- recently-listened/lambda_function.py
+++ recently-listened/lambda_function.py
@@ -5,10 +5,7 @@ from botocore.exceptions import ClientError
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from flask import Flask
-from flask import jsonify
 import awsgi
-
-
 
 # Thinking of using a Flask application to navigate the authorization
 app = Flask(__name__)
@@ -17,6 +14,30 @@ app = Flask(__name__)
 # This is the link to the Lambda https://ebc2llbzrd4v43kqzwc6ctw2cy0jqjzb.lambda-url.us-east-2.on.aws/
 def lambda_handler(event, context):
     # Some additional mappings are required because they don't exist in the new version of AWS
+    secret_name = 'mickeys-marvels/spotipy'
+    region = 'us-east-2'
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region
+    )
+
+    CLIENT_ID = ""
+    CLIENT_SECRET = ""
+    REDIRECT_URI = ""
+
+    try:
+        response = client.get_secret_value(SecretId=secret_name)
+        secret_string = response['SecretString']
+
+        CLIENT_ID = secret_string[14:46]
+        CLIENT_SECRET = secret_string[65:98]
+        REDIRECT_URI = secret_string[115:len(secret_string) - 2]
+
+    except ClientError as e:
+        print("Something went wrong, printing error information")
+        print(e)
+
     event['httpMethod'] = event['requestContext']['http']['method']
     event['path'] = event['requestContext']['http']['path']
     event['queryStringParameters'] = event.get('queryStringParameters', {})
@@ -25,5 +46,4 @@ def lambda_handler(event, context):
 # May have to use some requests to get the authorization working
 @app.route("/")
 def index():
-    return jsonify(status=200, message='Hello from Flask app in Lambda!')
-
+    return "This is a test"
