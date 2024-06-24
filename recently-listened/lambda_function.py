f52b8b6114a569e4621c30cacf98660812ede4d9
Meekyle Mondal <amond049@uottawa.ca>
2024-06-24 00:37:13 -0400

    Trying to use this awsgi library

--- recently-listened/lambda_function.py
+++ recently-listened/lambda_function.py
@@ -5,6 +5,10 @@ from botocore.exceptions import ClientError
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from flask import Flask
+from flask import jsonify
+import awsgi
+
+
 
 # Thinking of using a Flask application to navigate the authorization
 app = Flask(__name__)
@@ -12,14 +16,10 @@ app = Flask(__name__)
 
 # This is the link to the Lambda https://ebc2llbzrd4v43kqzwc6ctw2cy0jqjzb.lambda-url.us-east-2.on.aws/
 def lambda_handler(event, context):
-    app.run('localhost', 8080)
-    print("Running Flask application")
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda! This is the second one')
-    }
+    return awsgi.response(app, event, context)
 
 # May have to use some requests to get the authorization working
 @app.route("/")
-def test():
-    return "Hello World!"
+def index():
+    return jsonify(status=200, message='Hello from Flask app in Lambda!')
+
