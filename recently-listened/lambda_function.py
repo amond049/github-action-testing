cebb26d6243dbd6026e788bab4e44e18ddb8d6ce
Meekyle Mondal <amond049@uottawa.ca>
2024-06-28 00:33:18 -0400

    Finally got the recently listened tracks Lambda working

--- recently-listened/lambda_function.py
+++ recently-listened/lambda_function.py
@@ -1,65 +1,43 @@
-# Will need to retrieve the credentials from secrets manager
-import boto3
 import json
-from botocore.exceptions import ClientError
-import spotipy
-from spotipy.oauth2 import SpotifyOAuth
-from flask import Flask, redirect, request
-import awsgi
+import os
+# The name of the folder in the layer MUST BE python!
+import requests
+from requests.auth import HTTPBasicAuth
 
-# Thinking of using a Flask application to navigate the authorization
-app = Flask(__name__)
-
-# Setting it to None for the time being
-sp = None
-
-
-# Production Development
-CLIENT_ID = ""
-CLIENT_SECRET = ""
-REDIRECT_URI = ""
-scope = "user-read-recently-played"
+# Some global variables
+CLIENT_ID = os.environ['CLIENT_ID']
+CLIENT_SECRET = os.environ['CLIENT_SECRET']
+REDIRECT_URI = os.environ['REDIRECT_URI']
+REFRESH_TOKEN = os.environ['REFRESH_TOKEN']
 
 
-# Production Development
+def get_recently_played(token):
+    # This is where the most recent 20 tracks will be retrieved
+    response = requests.get(
+        "https://api.spotify.com/v1/me/player/recently-played",
+        params={'limit': 20},
+        headers={'Authorization': 'Bearer ' + token}
+    )
 
-secret_name = 'mickeys-marvels/spotipy'
-region = 'us-east-2'
-session = boto3.session.Session()
-client = session.client(
-    service_name='secretsmanager',
-    region_name=region
-)
+    return response
 
-try:
-    response = client.get_secret_value(SecretId=secret_name)
-    secret_string = response['SecretString']
 
-    CLIENT_ID = secret_string[14:46]
-    CLIENT_SECRET = secret_string[65:97]
-    REDIRECT_URI = secret_string[115:len(secret_string) - 2]
+def refresh_token():
+    # This is where the token refreshing functionality will go
+    response = requests.post(
+        "https://accounts.spotify.com/api/token",
+        params={'grant_type': 'refresh_token', 'refresh_token': REFRESH_TOKEN},
+        auth=HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET),
+        headers={'Content-Type': 'application/x-www-form-urlencoded'}
+    )
 
-except ClientError as e:
-    print("Something went wrong, printing error information")
-    print(e)
+    return response.json()["access_token"]
+
 
 def lambda_handler(event, context):
-    # Some additional mappings are required because they don't exist in the new version of AWS
-    event['httpMethod'] = event['requestContext']['http']['method']
-    event['path'] = event['requestContext']['http']['path']
-    event['queryStringParameters'] = event.get('queryStringParameters', {})
-    return awsgi.response(app, event, context)
-
-
-auth_manager = SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, scope=scope, redirect_uri=REDIRECT_URI)
-@app.route("/")
-def index():
-    return redirect(auth_manager.get_authorize_url())
-
-@app.route("/callback")
-def callback():
-    token = auth_manager.get_access_token(request.args.get("code"), as_dict=False)
-    # Needed to add the token to the Spotify instance because Lambda cannot write to the cache (without VPC, which is a paid service)
-    sp = spotipy.Spotify(auth_manager=auth_manager, auth=token)
-    result = sp.current_user_recently_played(limit=20)
-    return result
+    new_token = refresh_token()
+    tracks = get_recently_played(new_token)
+    return {
+        'statusCode': 200,
+        'body': json.dumps(tracks.json())
+    }
