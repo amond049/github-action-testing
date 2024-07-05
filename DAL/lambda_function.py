96b698235f3b99cfbf85354bd2fc42266f3616ff
Meekyle Mondal <amond049@uottawa.ca>
2024-07-05 19:47:15 -0400

    Creating the Lambda to get the 9 most recent album reviews

--- DAL/lambda_function.py
+++ DAL/lambda_function.py
@@ -1,20 +1,20 @@
 import json
 import boto3
 
+
 def lambda_handler(event, context):
     db = boto3.resource('dynamodb')
-
     table = db.Table('mickeys-marvels')
 
-    response = table.get_item(
-        Key={
-            'entryID': 0
-        }
+    response = table.scan(
+        Select='ALL_ATTRIBUTES',
+        Limit=9
     )
-
-    print(response['Item'])
 
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda and Github Actions with the Lambda working as intended!')
+        'headers': {
+            'Access-Control-Allow-Origin': '*'
+        },
+        'body': response
     }
